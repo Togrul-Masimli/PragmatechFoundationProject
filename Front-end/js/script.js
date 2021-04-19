e2932368033eb40af0db86f684163fd048d32777
Togrul Masimli <togrul.mesimli02@gmail.com>
2021-04-19 21:01:49 +0400

    comment section added

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,1 +1,10 @@
-const accordionButton = document.getElementsByClassName("accordion");
+document.querySelector('.accordion').addEventListener("click", () => {
+    let accordion = document.querySelector('.accordion');
+    let comment = document.querySelector('.comment');
+
+    if (comment.style.maxHeight) {
+        comment.style.maxHeight = null;
+    } else {
+        comment.style.maxHeight = comment.scrollHeight + "px";
+    }
+});
