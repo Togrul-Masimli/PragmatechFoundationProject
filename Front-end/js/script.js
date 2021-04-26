098b0d0e2355fe4ee6d7c90266a4fcc005ed2dde
Togrul Masimli <togrul.mesimli02@gmail.com>
2021-04-26 13:51:53 +0400

    to-top animation added

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -10,13 +10,36 @@
 });
 */
 
-document.querySelector('.col-6').addEventListener('click', (e) => {
-    if (e.target.getAttribute('class') == 'accordion') {
+let accordion = document.querySelectorAll('.accordion');
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', () => {
         let comment = document.querySelector('.comment');
         if (comment.style.maxHeight) {
             comment.style.maxHeight = null;
         } else {
             comment.style.maxHeight = comment.scrollHeight + "px";
         }
+    })
+}
+
+
+/*
+document.querySelector('.col-6').addEventListener('click', (e) => {
+    if (e.target.getAttribute('class') == 'accordion') {
+        if (document.querySelector('.comment').style.maxHeight) {
+            document.querySelector('.comment').style.maxHeight = null;
+        } else {
+            document.querySelector('.comment').style.maxHeight = document.querySelector('.comment').scrollHeight + "px";
+        }
     }
-})
+});
+*/
+
+
+document.querySelector('.to-top').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+});
