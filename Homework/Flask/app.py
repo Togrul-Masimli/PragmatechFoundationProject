4ec57e016941a859614b5535813f56aa2270f2ca
Togrul Masimli <togrul.mesimli02@gmail.com>
2021-05-23 20:53:11 +0400

    Learning Flask

--- Homework/Flask/app.py
+++ Homework/Flask/app.py
@@ -1,7 +1,9 @@
 from logging import debug
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5f47fc632a585184688be1969d20bfcb'
 
 posts = [
     {
@@ -26,6 +28,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
